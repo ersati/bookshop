12b3315ebe058958c78ae8267d019708d0999054
ersati <piotrp92@gmail.com>
2020-06-23 18:29:38 +0100

    add author, title and image ,styles

--- index.js
+++ index.js
@@ -1,38 +1,39 @@
 import './style.css';
 
-const API_URL = 'https://cors-anywhere.herokuapp.com/https://www.goodreads.com/book/auto_complete?format=json&q=harry%20potter'
+const API_URL = 'https://cors-anywhere.herokuapp.com/https://www.goodreads.com/book/auto_complete?format=json&q='
 
 const input = document.querySelector('.form__input');
 const btn = document.querySelector('#form__button');
 const results = document.querySelector('.results');
 
 
-
 const generateDatatoHTML = (data) => {
-console.log(data)
+results.innerHTML = '';
 data.forEach(book => {
-  console.log(book.author.name)
   const li = document.createElement('li');
-  const html = `<p>${book.author.name}</p>`
-  li.innerHTML = html
   
+  const html = `
+  <img src='${book.imageUrl}' alt='${book.bookTitleBare}' class='entry__image'/>
+  <p class="entry__name-author">${book.author.name}</p>
+  <p class="entry__name">${book.title}</p>
+  <button class="btn__item"><a href="${book.description.fullContentUrl}">Read More</a></button>
+  `
+
+  li.innerHTML = html
   results.appendChild(li)
 })
 }
 
 const fetchData = (search) => {
-fetch(`${API_URL}`)
+fetch(`${API_URL}${search}`)
 .then(response => response.json())
 .then(data => {
-  // console.log('Success:', data);
   generateDatatoHTML(data);
 })
 .catch((error) => {
   console.error('Error:', error.message);
 });
 }
-
-
 
 btn.addEventListener('click', (e) =>{
   e.preventDefault()
